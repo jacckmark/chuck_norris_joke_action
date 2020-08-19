ee6a257f0e7f72a0a21ad144ca5d4ccdb95e7e78
Jack Mark <jacckmark@gmail.com>
2020-08-19 15:03:04 +0200

    initial changes

---
+++ index.js
@@ -0,0 +1,44 @@
+const github = require("@actions/github");
+const core = require("@actions/core");
+const axios = require('axios').default;
+let nodemailer = require('nodemailer');
+
+
+async function run() {
+    const sendingEmailAddress = core.getInput("sendingEmailAddress");
+    const sendingEmailPass = core.getInput("sendingEmailPass");
+    const sendingEmailProvider = sendingEmailAddress.find(/(?<=@)\w*/);
+    const receivingEmailAddress = core.getInput("receivingEmailAddress");
+    let transporter = nodemailer.createTransport({
+        service: sendingEmailProvider,
+        auth: {
+            user: sendingEmailAddress,
+            pass: sendingEmailPass
+        }
+    });
+
+    let mailOptions = {
+        from: sendingEmailAddress,
+        to: receivingEmailAddress,
+        subject: 'Some secrets for you',
+        text: await getChuckJoke()
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (error) {
+            console.error(err);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
+async function getChuckJoke() {
+    axios.get('https://api.chucknorris.io/jokes/random').then((resp) => {
+        return resp.value;
+    }).catch(err => {
+        console.error(err)
+    });
+}
+
+run();
