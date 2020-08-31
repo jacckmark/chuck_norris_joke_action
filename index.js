87e4cd40247b9c772a186f006276e38d95a851fa
Jack Mark <jacckmark@gmail.com>
2020-08-31 15:48:57 +0200

    chang

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const github = require("@actions/github");
 const core = require("@actions/core");
 const axios = require('axios').default;
-const { eventName, payload, repo } = github.context;
 
 async function run() {
     const githubAccessToken = core.getInput("githubAccessToken");
@@ -23,11 +22,14 @@ async function getYodaTranslation(textToTranslate, translationApiToken) {
 
 
 async function replaceGithubComments(translateApiToken, githubToken) {
+    const { eventName, payload, repo } = github.context;
     const octokit = github.getOctokit(githubToken);
     if (eventName === 'issue_comment' || eventName === 'pull_request_review_comment') {
         // console.log(payload);
         const comment = payload.comment.body;
+        console.log('111111111111111111111111111111111111111111111111111111');
         const result = await getYodaTranslation(comment, translateApiToken);
+        console.log('222222222222222222222222222222222222222222222222222222');
         console.log(result);
         octokit.issues
             .updateComment({ ...repo, comment_id: payload.comment.id, comment })
